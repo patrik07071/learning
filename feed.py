2f0f19c836cfcd996bda1aef7a69c50dac93428d
Patrik <80455248+patrik07071@users.noreply.github.com>
2024-10-10 06:16:35 +0000

    .

--- feed.py
+++ feed.py
@@ -36,6 +36,5 @@ for item in yaml_data['item']:
       'length': item['length']
     })
 
-# 
 output_tree = xml_tree.ElementTree(rss_element)
 output_tree.write('podcast.xml', encoding='UTF-8', xml_declaration=True)
